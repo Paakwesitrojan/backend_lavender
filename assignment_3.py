dc17578442427cc32e5fb0c6d5f29ddbc272b2b8
Paakwesitrojan <robertdarko21@gmail.com>
2025-01-27 18:05:38 +0000

    Assignment Number Three

---
+++ assignment_3.py
@@ -0,0 +1,21 @@
+sentence=("For strong and healthy growth choose NIDO")
+
+print(sentence)
+
+print(sentence.split(" "))
+
+print(sentence.split(" ")[0])
+
+print(sentence.split(" ")[6])
+
+print(sentence[0:3])
+
+print(sentence[38:])
+
+print(sentence[::-1])
+
+print(sentence.upper())
+
+print(sentence.lower())
+
+print(sentence.replace (" ", "-"))
